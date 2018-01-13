1964c4bcaf1668ac595e64fad13b2ad9bab2145c
Unknown <igwal11200@gmail.com>
2018-01-13 22:13:03 +0100

    THOMAS FALCONE : Le prix total s'actualise automatiquement.
    Me remerciez pas

--- view/reservation/addReservationPlaces.php
+++ view/reservation/addReservationPlaces.php
@@ -1,5 +1,6 @@
 <?php if (isset($error)): echo $error; endif;
 	  echo "il reste ". ModelLouer::getNombrePlaceRestante(Conf::$idFestival) ." places disponilbes !";
+      $prixUnitaire = ModelFestival::getPrixUnitaire(Conf::$idFestival);
 		$dateCourante = new DateTime();
 		$dateRelance = new DateTime("+ 2 weeks");
 ?>
@@ -7,13 +8,13 @@
 	<fieldset class="form-infos">
 		<?php foreach ($tabZone as $zone) {
 			echo 'Zone: '. $zone->getLibelleZone() . '<label>Nombre de places :
-			<input type="number" placeholder="1" name="nbPlace[]" required/></label>';
+			<input id="nbplace" type="number" placeholder="1" onfocusout="calcul()" name="nbPlace[]" required/></label>';
 		} ?>
 
 		<label>Jeux:
 			<!-- numJeu[] = tableau car il peut y avoir plusieurs jeux sélectionnés
 				 multiple="multiple" car on peut sélectionner plusieurs jeux-->
-			<select name="numJeu[]" multiple="multiple" required>
+            <select name="numJeu[]" multiple="multiple" required>
 				<?php
 					foreach ($listeJeux as $jeux) {
 		           		echo '<option value="'. htmlspecialchars($jeux->getNumJeu()). '">' . htmlspecialchars($jeux->getLibelleJeu()) .'</option>';
@@ -32,17 +33,31 @@
 			<input type="date" value="<?php echo $dateRelance->format('Y-m-d');?>" name="dateRelance" required/></label>
 
 		<label>Prix en euros :
-			<input type="number" placeholder="20" name="prix" required/></label>
-
+			<input id="affiche" type="number" placeholder="20" value="<?php echo $prixUnitaire ?>" name="prix" required/></label>
+        
+        <button onclick="calcul()" value="refresh le prix maggle"> Nique </button>
 		<label>L'editeur se déplace-t-il à l'évènement ? :
 			<input type="checkbox" placeholder="Oui" name="deplacement"/></label>
 
 		<?php foreach ($listeZone as $zone) {
 			echo '<input type="hidden" value="'. $zone .'" name="idZone[]" required />';
 		}?>
-		<input type="hidden" value="<?php echo $numEditeur; ?>" name="numEditeur" required />
+		<input type="hidden" value="<?php echo $numEditeur; ?>" name="numEditeur" required / >
     </fieldset>
 	<fieldset class="form-action">
 			<input class="form-bouton" type="submit" name="submit" value="Suivant" />
 	</fieldset>
 </form>
+<div class="none">
+    <p id="prix"><?php echo($prixUnitaire)?></p>
+</div>
+
+
+<script>
+    function calcul(){
+        var prix = document.getElementById("prix").innerHTML;
+        var nbTable = document.getElementById("nbplace").value;
+        console.log(nbTable);
+        document.getElementById("affiche").value = prix * nbTable;
+    }
+</script>
