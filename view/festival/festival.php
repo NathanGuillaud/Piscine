1d58b39711c875664c83674f0550b448415c1de9
Alexis Sanchez <alexis.sanchez01@etu.umontpellier.fr>
2018-01-15 01:19:07 +0100

    Update festival.php

--- view/festival/festival.php
+++ view/festival/festival.php
@@ -3,7 +3,6 @@
 		<?php echo $error; ?>
 	</p>
 <?php endif;
-<<<<<<< Updated upstream
 	  echo '<br><div class="infos">
             <div class="submenu">
                 <form method="post" action="index.php?controller=festival&action=changeFestival">
@@ -12,18 +11,19 @@
                 <select name="idFestival">';
                 
                    foreach (ModelFestival::getAllFestivals() as $festival) {
-	           			echo '<option value="'. htmlspecialchars($festival->getIdFestival()). '">' . htmlspecialchars($festival->getAnneeFestival()) .'</option>';
+                   		if($_SESSION['idFestival'] == $festival->getIdFestival()){
+                   			echo '<option value="'. htmlspecialchars($festival->getIdFestival()). '" selected>' . htmlspecialchars($festival->getAnneeFestival()) .'</option>';
+                   		}else{
+                   			echo '<option value="'. htmlspecialchars($festival->getIdFestival()). '">' . htmlspecialchars($festival->getAnneeFestival()) .'</option>';
+                   		}	           			
 	           		}
                 
                 echo "</select> <div><button type='submit' class='edit-button'>Ok</button>
                 </form>
             </div><br>";
-	  echo "<br><h2> Paramètres du festival : </h2>";
-=======
-	  echo "<br><div class='infos'> <h2> Paramètres du festival : </h2>";
->>>>>>> Stashed changes
-	  if(!empty(ModelFestival::getFestivalById(1))){
-	  		$festival = ModelFestival::getFestivalById(1);
+	  echo "<br><h2> Paramètres du festival " . ModelFestival::getFestivalById($_SESSION['idFestival'])->getAnneeFestival() . "</h2>";
+	  if(!empty(ModelFestival::getFestivalById($_SESSION['idFestival']))){
+	  		$festival = ModelFestival::getFestivalById($_SESSION['idFestival']);
 	  		echo '<p>Salle: ' . htmlspecialchars($festival->getNomSalle()) . '</p><hr/><p> Places: ' . htmlspecialchars($festival->getnbTotalPlace()) . ' </p><hr/><p>Prix: ' . htmlspecialchars($festival->getPrixUniTable()) . '<hr/></p>' ;
 	  	 	echo "<div><a  class='edit-button' href='index.php?controller=festival&action=update&idFestival=1'>Modifier les paramètres du festival</a></div>";
 	  	}else{
