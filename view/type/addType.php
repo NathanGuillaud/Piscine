987836780fe3065ad171a645d9d7349f944172aa
Lucas Gonçalves <igwal11200@gmail.com>
2018-01-15 10:20:18 +0100

    8 Ajout du style ajout type de jeux

--- view/type/addType.php
+++ view/type/addType.php
@@ -1,9 +1,10 @@
-<form method="post" action="index.php?controller=type&action=registerType">
-	<fieldset class="form-infos">
-	    <label>Libelle Type:
-	    	<input type="text" placeholder="Jeu enfant" name="libelleType" required /></label>
-    </fieldset>
-	<fieldset class="form-action">
-			<input class="form-bouton" type="submit" name="submit" value="Enregistrer" />
-	</fieldset>
-</form>
+<div class="infos">
+    <h2>Creation d'un nouveau type de jeu : </h2>
+    <form method="post" action="index.php?controller=type&action=registerType">
+        <fieldset class="form-infos">
+            <label>Libelle Type:
+                <input type="text" placeholder="Jeu enfant" name="libelleType" required /></label>
+        </fieldset>
+                <input class="edit-button-save" type="submit" name="submit" value="Enregistrer" />
+    </form>
+    </div>
